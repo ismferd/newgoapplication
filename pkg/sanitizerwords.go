88c893cd0ad10bfd9a5fdbba150ed3de4d384bc9
ismferd <fernandez.molina.ismael@gmail.com>
2022-02-13 19:44:57 +0100

    pk

---
+++ pkg/sanitizerwords.go
@@ -0,0 +1,18 @@
+package sanitizerwords
+
+import (
+	"strings"
+	"unicode"
+)
+
+func SanitizerWords(s string) string {
+	r := []rune(s)
+	var cleanWord string
+	for i := 0; i < len(r); i++ {
+		if unicode.IsLetter(r[i]) {
+			cleanWord = cleanWord + string(r[i])
+		}
+	}
+	cleanWord = strings.ReplaceAll(cleanWord, " ", "")
+	return strings.ToLower(cleanWord)
+}
